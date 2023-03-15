6561a542b42de6b31da11bbeb550af06390937c5
Jeison Tomio <medriase@gmail.com>
2023-03-15 03:09:15 -0300

    error RuntimeError

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "../styles/pages/home"
 import 'keen-slider/keen-slider.min.css'
 import Image from "next/image"
-
+import Link from 'next/link'
 
 import { stripe } from '@/lib/stripe'
 import Stripe from 'stripe'
@@ -14,7 +14,7 @@ interface HomeProps {
     id: string;
     name: string;
     imageUrl: string;
-    price?: number;
+    price: string;
   }[]
 }
 
@@ -34,14 +34,21 @@ export default function Home({ products }: HomeProps) {
       <HomeContainer ref={sliderRef} className='keen-slider' >
         {products.map(product => {
           return (
-            <Product key={product.id} className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt={""} />
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+            >
+              <Product
+                className="keen-slider__slide"
+              >
+                <Image src={product.imageUrl} width={520} height={480} alt={""} />
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           )
         })}
 
